f0e8981ea46fcf48ec8ac098d17988a4070d914a
langdonglei <langdonglei@sina.com>
2019-07-24 12:03:02 +0800

    m

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/deploy',function(){
-    $a=file_get_contents("php://input");
-    var_dump($a);
+Route::any('/deploy',function(Illuminate\http\Request $request){
+    $s1=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+    $s2='sha1='.hash_hmac('sha1',file_get_contents('php://input'),$_ENV['GITHUB_WEBHOOK_SECRET']);
+    if($s1==$s2){
+        $path = base_path();
+        $proc = proc_open("cd $path && git pull", [1=>['pipe','w'],2=>['pipe','w']], $pipes);
+        echo stream_get_contents($pipes[1]);
+        echo stream_get_contents($pipes[2]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        proc_close($proc);
+    }
+
 });
