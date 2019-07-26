56e701165732f0910af8a6fe9cff9df6448adf16
langdonglei <langdonglei@sina.com>
2019-07-26 16:03:14 +0800

    后台登录系统

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,12 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('blog.title') }} 管理后台</title>
-
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
     @yield('styles')
 
 </head>
@@ -33,8 +29,7 @@
 <main class="py-4">
     @yield('content')
 </main>
-{{--<script src="{{ asset('js/app.js') }}"></script>--}}
-
+<script src="{{ asset('js/app.js') }}"></script>
 @yield('scripts')
 </body>
 </html>
